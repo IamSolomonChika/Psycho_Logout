29248c47866196c7d0fc9530d23f72c3a3410b35
Solomon Chika <solomon.chika1@gmail.com>
2024-11-21 10:07:09 +0100

    P1- Finishing Touch

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
 })
